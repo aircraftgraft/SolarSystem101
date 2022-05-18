0bf14d179dee1613ef2fd9a723e28e7d8aa3308c
aircraftgraft <aircraftgraft@gmail.com>
2022-05-18 19:42:45 -0400

    Solar System Basic Ready

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.company.SolarSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun = "1";
+        FactorX.Moon = "4";
+        FactorX.Planet = "2";
+        FactorX.Star = "10000";
+    }
+}
